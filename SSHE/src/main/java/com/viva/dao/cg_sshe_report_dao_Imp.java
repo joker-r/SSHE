4495d57cb8b75dd6364389aa5bd5fc5e97ef6101
joker-r <ali.sanu@hotmail.com>
2019-07-26 15:30:29 +0530

    added daoimp

--- SSHE/src/main/java/com/viva/dao/cg_sshe_report_dao_Imp.java
+++ SSHE/src/main/java/com/viva/dao/cg_sshe_report_dao_Imp.java
@@ -2,15 +2,18 @@ package com.viva.dao;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.ListIterator;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 
 import com.viva.entity.cg_sshe_report;
 @Repository("reportdao")
@@ -27,6 +30,17 @@ public class cg_sshe_report_dao_Imp implements cg_sshe_report_dao{
 		return 0;
 	}
 
-	
+	@Override
+	public List<cg_sshe_report> generateReport(String cpid) {
+		// TODO Auto-generated method stub
+		String hql="select distinct(product_id),count(status) from cg_sshe_report where cpid=:ecpid group by product_id";
+		Query qry=em.createQuery(hql);
+		qry.setParameter("ecpid", cpid);
+		List<cg_sshe_report> lst=qry.getResultList();
+		System.out.println(lst);
+		
+		return lst;
+	}
 
+	
 }
